20de69795f6ebe02d1c9020cc2d78058b31b52a4
lipanovichsol <sol.lipanovich@gmail.com>
2018-10-24 13:28:31 -0300

    profesor-commit

---
+++ src/profesor/profesor.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core'; 
+
+@Component({
+    selector: 'app-profesor',
+    templateUrl: './profesor.component.html'
+})
+
+export class ProfesorComponent{
+    public nombre: string; 
+    public edad: number;
+    public casado: boolean;
+    public trabajos: Array<any> = ['Profesor','Webdev','Barrendero'];
+
+    constructor(){
+        this.nombre='Sol';
+        this.edad= 23;
+        this.casado=false;
+    }
+}
+
